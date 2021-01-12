915a502ba1482a8ce6b45939e7f5662b13c5cebf
Michael Zwyssig <michael.zwyssig@migrosonline.ch>
2021-01-12 15:13:22 +0100

    flighjts oin homepage

--- apps/mfe1/src/app/flights/flights.module.ts
+++ apps/mfe1/src/app/flights/flights.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FlightsSearchComponent } from './flights-search/flights-search.component';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FlightsSearchComponent } from './flights-search/flights-search.component';
 import { FLIGHTS_ROUTES } from './flights.routes';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forChild(FLIGHTS_ROUTES)
-  ],
-  declarations: [
-    FlightsSearchComponent
-  ]
+	imports: [
+		CommonModule,
+		RouterModule.forChild(FLIGHTS_ROUTES)
+	],
+	exports: [
+		FlightsSearchComponent
+	],
+	declarations: [
+		FlightsSearchComponent
+	]
 })
 export class FlightsModule { }
--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FlightsModule } from '../../../mfe1/src/app/flights/flights.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { APP_ROUTES } from './app.routes';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(APP_ROUTES)
-  ],
+	imports: [
+		BrowserModule,
+		RouterModule.forRoot(APP_ROUTES),
+		FlightsModule
+	],
   declarations: [
     AppComponent,
     HomeComponent
