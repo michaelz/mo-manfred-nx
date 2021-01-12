d090896bb5dea85104d61dca1bb4bd99d67015f5
Michael Zwyssig <michael.zwyssig@migrosonline.ch>
2021-01-12 11:37:24 +0100

    prod configs

--- apps/mfe1/webpack.prod.config.js
+++ apps/mfe1/webpack.prod.config.js
@@ -1,1 +1,26 @@
-module.exports = require('./webpack.config');
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+	output: {
+		publicPath: "/",
+		uniqueName: "mfe1"
+	},
+	optimization: {
+		// Only needed to bypass a temporary bug
+		runtimeChunk: false
+	},
+	plugins: [
+		new ModuleFederationPlugin({
+
+			// For remotes (please adjust)
+			name: "mfe1",
+			library: { type: "var", name: "mfe1" },
+			filename: "remoteEntry.js",
+			exposes: {
+				'./Module': './apps/mfe1/src/app/flights/flights.module.ts',
+			},
+
+			shared: ["@angular/core", "@angular/common", "@angular/router"]
+		})
+	],
+};
--- apps/shell/webpack.prod.config.js
+++ apps/shell/webpack.prod.config.js
@@ -1,1 +1,20 @@
-module.exports = require('./webpack.config');
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+	output: {
+		publicPath: "/",
+		uniqueName: "shell"
+	},
+	optimization: {
+		// Only needed to bypass a temporary bug
+		runtimeChunk: false
+	},
+	plugins: [
+		new ModuleFederationPlugin({
+			remotes: {
+				'mfe1': "mfe1@/remoteEntry.js"
+			},
+			shared: ["@angular/core", "@angular/common", "@angular/router"]
+		})
+	],
+};
